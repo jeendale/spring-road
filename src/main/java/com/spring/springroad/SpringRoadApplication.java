9fb33513058239d70529cbe6e482751e463c65dc
jeendale <jeekoo7203@gmail.com>
2023-12-05 17:14:05 +0900

    Initial commit

---
+++ src/main/java/com/spring/springroad/SpringRoadApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRoadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/springroad/SpringRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
